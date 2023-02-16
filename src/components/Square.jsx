57020f32fba7249d2fec4c6161a925a0edfb7a9c
Satyam <satus2311@gmail.com>
2023-02-16 21:22:43 +0530

    Added a new player

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 const Square = ({ value, onClick }) => {
-  console.log(onClick);
   return (
     <button type='button' className='square' onClick={onClick}>
       {value}
