358151f27bcb775a15630859952f019d8f204336
Satyam <satus2311@gmail.com>
2023-02-22 13:14:44 +0530

    Added corner balls

--- src/App.jsx
+++ src/App.jsx
@@ -75,6 +75,7 @@ const App = () => {
       </button>
       <h2 style={{ fontWeight: 'normal' }}>Current Game History</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className='bg-balls' />
     </div>
   );
 };
